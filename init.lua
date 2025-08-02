ce03ead085260f383ff225cc214784fa6ab793f8
ysl2 <songliyu97@gmail.com>
2025-08-02 21:28:21 +0800

    Change lazy.nvim's configs.

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
+  local repo = "git@github.com:folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
@@ -69,6 +69,10 @@ require("lazy").setup({
         "ftplugin",
       },
     },
+  },
+  change_detection = { notify = false },
+  git = {
+    url_format = "git@github.com:%s.git",
   },
 })
 
