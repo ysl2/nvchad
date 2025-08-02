df5d93f21c8844789a47dc2792f3c3cc62ad0d79
ysl2 <songliyu97@gmail.com>
2025-08-03 02:09:28 +0800

    Add markdown lsp support (but marksman seems not work).

--- lua/plugins/nvim_lsp.lua
+++ lua/plugins/nvim_lsp.lua
@@ -1,16 +1,56 @@
+-- Ref: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/lang/markdown.lua
 return {
-  -- {
-  --   "stevearc/conform.nvim",
-  --   -- event = 'BufWritePre', -- uncomment for format on save
-  --   opts = require "configs.conform",
-  -- },
-
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
   { import = "nvchad.blink.lazyspec" },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      github = {
+        download_url_template = "https://ghfast.top/https://github.com/%s/releases/download/%s/%s",
+      },
+      ensure_installed = { "marksman", "markdownlint-cli2", "markdown-toc" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local servers = { "marksman" }
+      vim.lsp.enable(servers)
+    end
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = { "markdownlint-cli2" },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find "<!%-%- toc %-%->" then
+                return true
+              end
+            end
+          end,
+        },
+        ["markdownlint-cli2"] = {
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
+        },
+      },
+      formatters_by_ft = {
+        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+      },
+    },
+  },
 }
